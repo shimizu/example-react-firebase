b4a253612a2290998ebfe4f69a3cae7be011b1a8
shimizu <xxshimizuxx@gmail.com>
2020-02-14 13:46:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from "react";
 import './App.css';
+
+import firebase from "firebase";
+import { firebaseConfig } from './firebase/config';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+import  Hello  from "./page/login";
+import  HelloSomeone  from "./page/hellowsomeone";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+//firebase初期化
+firebase.initializeApp(firebaseConfig);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID
+  ]
+};
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Hello />
+        </Route>
+        <Route path="/hello/:name" exact>
+          <HelloSomeone />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB6epRV2HFg9DAUIzYJ44RGkglG-KbX9hE",
+    authDomain: "geosp-hackathon2020.firebaseapp.com",
+    databaseURL: "https://geosp-hackathon2020.firebaseio.com",
+    projectId: "geosp-hackathon2020",
+    storageBucket: "geosp-hackathon2020.appspot.com",
+    messagingSenderId: "812320713653",
+    appId: "1:812320713653:web:b173237c776679294fc7fc"
+  };
